0a55567dcdd8c495f5d92f8a00b408e740ec5aae
Mike Smith <michaelnsmith09@gmail.com>
2024-12-08 15:29:09 -0700

    third commit

--- super-review/age-checker/js/main.js
+++ super-review/age-checker/js/main.js
@@ -1,13 +1,63 @@
 //Create a conditonal that checks their age
 //If under 16, tell them they can not drive
-//If under 18, tell them they can't hate from outside the club, because they can't even get in
-//If under 21, tell them they can not drink
-//If under 25, tell them they can not rent cars affordably
-//If under 30, tell them they can not rent fancy cars affordably
-//If under over 30, tell them there is nothing left to look forward too
 
+//If under 18, tell them they can't hate from outside the club, because they can't even get in
+
+//If under 21, tell them they can not drink
+
+//If under 25, tell them they can not rent cars affordably
+
+//If under 30, tell them they can not rent fancy cars affordably
+
+//If under over 30, tell them there is nothing left to look forward too
+let age = 25
+
+if (age < 16){
+    console.log("they can not drive")
+} else if (age < 18) {
+    console.log("they can't hate from outside the club, because they can't even get in")
+} else if (age < 21) {
+    console.log("they can not drink")
+} else if (age < 25) {
+    console.log("they can not rent cars affordably")
+} else if (age < 30) {
+    console.log("they can not rent fancy cars affordably")
+} else if (age < 30 || age > 30){
+    console.log("there is nothing left to look forward too")
+} else {
+    console.log("Something cool")
+}
 
 //--- Harder
 //On click of the h1
+
 //Take the value from the input
+
 //Place the result of the conditional in the paragraph
+
+
+let ageAbove = Number(document.querySelector('input').value)
+
+document.querySelector('h1').addEventListener('click'ageCheck)
+
+let p = document.querySelector('p')
+
+function ageCheck(){
+    let ageAvove = Number(document.querySelector('input').value)
+
+    if (ageAbove < 16){
+        p.innerText = "they can not drive"
+    } else if (ageAbove < 18) {
+        p.innerText = "they can't hate from outside the club, because they can't even get in"
+    } else if (ageAbove < 21) {
+        p.innerText = "they can not drink"
+    } else if (ageAbove < 25) {
+        p.innerText = "they can not rent cars affordably"
+    } else if (ageAbove < 30) {
+        p.innerText = "they can not rent fancy cars affordably"
+    } else if (ageAbove < 30 || age > 30){
+        p.innerText = "there is nothing left to look forward too"
+    } else {
+        p.innerText = "Something cool"
+    }
+}
--- super-review/variable-fun-time/js/main.js
+++ super-review/variable-fun-time/js/main.js
@@ -1,18 +1,32 @@
 //--- Easy
 //create a variable and assign it a number
+let num = 11;
 
 //minus 10 from that number
+num = num - 10;
 
 //print that number to the console
+console.log(num);
+
 
 //--- Medium
 //create a variable that holds a value from the input
+let input = document.querySelector('input').value
 
 //add 25 to that number
+input += 25;
 
 //alert that number
+alert(input)
+
 
 //--- Hard
 //create a variable that holds the h1
+const h1 = document.querySelector('h1')
 
 //add an event listener to that element that console logs the sum of the two previous variables
+h1.addEventListener('click', addTwoNums)
+
+function addTwoNums(){
+    console.log(num + input)
+}
